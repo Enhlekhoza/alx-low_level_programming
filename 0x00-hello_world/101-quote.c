59009b984a5fa9eca688ce94c01612983b4cc07d
Enhlekhoza <dzimbaelsa3@gmail.com>
2022-09-09 00:04:03 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-#include <stdio.h>                                                            
-#include <unistd.h>                                                           
+#include <unistd.h>    
+                                                       
 /**                                                                           
  * main - entry point                                                         
  *                                                                            
- * Description: prints and that piece..                                       
- *Return: 1 if sucessful                                                      
- */                                                                           
-int main(void)                                                                
+ * Description: prints Dora's quote
+*                                      
+ * Return: 1 ( sucessful)    
+*/
+
+int main(void)                                                                  
 {                                                                             
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
-n", 59);                                                                      
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);                                                                   
         return (1);                                                           
 }
