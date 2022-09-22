039169681586c4df51785f40380bfedccf9313b8
Enhlekhoza <dzimbaelsa3@gmail.com>
2022-09-22 04:08:52 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main. h"
+
+/**
+*_strcat concatenates the string pointed to by @src to
+*the end of the string pointed to by @dest
+*@dest: String that will be appended
+*@src: String to be concatenated upon
+*
+*Return: returns poiner to @dest
+*/
+
+char  *_strcat(char  *dest, char  *src)
+{
+
+               int index = 0, dest_len =
+
+             while (dest[index++])
+                           dest_lent+*;
+
+            for  (index = ; src[index]; index++)
+                    dest[dest len++] = src[index] ;
+
+return (dest) ;
+
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h" 
+
+/**
+* string_toupper - changes all lowercase letters to uppercase
+* @str: string to be changed 5 
+*
+* Return: address to the string
+*/ 
+	
+	 char *string_toupper(char *str) 
+{ 
+	int i = 0;
+	
+	 while (str[i] != '10")
+	 {
+	 	 if (str[i] >= 'a' && str[i] <= z), 
+	 	 str[i] -= 32; 
+	 	 i++;
+	 	 }
+	 	  return (str); 
+}
