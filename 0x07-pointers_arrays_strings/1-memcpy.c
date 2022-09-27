8f7f43034058392ac0468ffb0c6820c4d5b6a129
Enhlekhoza <dzimbaelsa3@gmail.com>
2022-09-27 05:34:09 -0700

    2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,60 +1,22 @@
 #include "main.h"
 
-#include <stddef.h>
-
 /**
+ * _memcpy - copies a memory area
+ * @dest: memory area to be copied to
+ * @src: memory area to be copied from
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to the copied memory block
+ */
 
-  * _strstr - finds the first occurence of needle in haystack
-
-  * @haystack: where we look
-
-  * @needle: what we look for
-
-  *
-
-  *
-
-  * Return: pointer to beginning of located substring or NULL
-
-  *
-
-  */
-
-char *_strstr(char *haystack, char *needle)
-
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i = 0;
 
-        int point;
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-
-        for (; *haystack; haystack++)
-
-        {
-
-                for (point = 0; needle[point]; point++)
-
-                {
-
-                        if (!(*(haystack + point)))
-
-                        {
-
-                                return (NULL);
-
-                        }
-
-                if (*(haystack + point) != needle[point])
-
-                        break;
-
-                }
-
-                if (needle[point] == '\0')
-
-                        return (haystack);
-
-        }
-
-        return (NULL);
-
+	return (dest);
 }
