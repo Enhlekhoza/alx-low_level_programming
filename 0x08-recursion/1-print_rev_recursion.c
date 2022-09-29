c50064fed3633b423fda0ed37d38b8aa0c17128e
Enhlekhoza <dzimbaelsa3@gmail.com>
2022-09-29 05:09:51 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
 /**
-
- * _print_rev_recursion - print a string in reverse
-
- * @s: the string
-
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be printed
+ *
  * Return: void
-
  */
-
 void _print_rev_recursion(char *s)
-
 {
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}#include "main.h"
 
-        if (!*s)
+/**
+ * _strlen_recursion - calculates the length of a string
+ * @s: string to be used
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
 
-                return;
-
-        if (*s)
-
-        {
-
-                _print_rev_recursion(s + 1);
-
-                _putchar(*s);
-
-        }
-
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return (sum);
 }
-
-
