544b00e1ffd1c10d09d0162fab9c3e9b0d9f6bb2
Enhlekhoza <dzimbaelsa3@gmail.com>
2022-09-30 14:55:21 -0700

    ehh

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,52 +1,33 @@
-#include <stdio.h>                                                                                                                   #include <stdlib.h>                                                                                                                 #include <ctype.h>                                                                                                                   #include <string.h>                                                                                                                                                                                                                                                      
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-/**                                                                                                                                 
+/**
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
 
- * check_num - check - string there are digit                                                                                       
-
- * @str: array str                                                                                                                   
-
- *                                                                                                                                   
-
- * Return: Always 0 (Success)                                                                                                       
-
- */                                                                                                                                 
-
-                                                                                                                                     
-
-int check_num(char *str)                                                                                                            
-
-                                                                                                                                     
-
-{                                                                                                                                    
-
-/*Declaring variables*/                                                                                                             
-
-unsigned int count;                                                                                                                  
-
-                                                                                                                                     
-
-count = 0;                                                                                                                          
-
-while (count < strlen(str)) /*count string*/                                                                                         
-
-                                                                                                                                     
-
-{                                                                                                                                    
-
-if (!isdigit(str[count])) /*check if str there are digit*/
-
-{                                                                                                                                    
-
-return (0);                                                                                                                          
-
-}                                                                                                                                    
-
-                                                                                                                                     
-
-count++;                                                                                                                            
-
-}                                                                                                                                    
-
-return (1);                                                                                                                          
-
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
